2099ec4c72daf96fe08a9996a70639f2d22cb13a
kisbayu <kisbayu.k.a@gmail.com>
2024-02-26 13:59:33 +0700

    add favico & logo

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,7 +10,7 @@
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+    <link rel="icon" href="<?= base_url('/public/favicon.ico'); ?>" type="image/ico">
     <title>Beverages Order</title>
 </head>
 
@@ -18,6 +18,6 @@
 
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #FEFEFE;">
         <div class="container justify-content-center">
-            <img src="<?= base_url('/public/asahimasPNG.png'); ?>" class="img" alt="logo" style="height: 8vh;">
+            <img src="<?= base_url('/public/asahimasPNG.png'); ?>" class="img" alt="logo" style="height: 3rem;">
         </div>
     </nav>
